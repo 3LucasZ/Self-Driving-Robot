3c0e35f59d51633911f73dc46e917bae2f7214de
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-09 01:16:10 -0700

    Update motor_controller.py

--- Modules/motor_controller.py
+++ Modules/motor_controller.py
@@ -15,27 +15,29 @@ class MotorController:
             self.BUS = smbus.SMBus(config['MOTOR_DRIVER']['I2C_ID'])
             self.DEVICE_ADDRESS = config['MOTOR_DRIVER']['ADDRESS']
             self.LEFT_MOTOR = config['MOTOR_DRIVER']['MOTORS']['LEFT']
-            self.RIGHT_MOTOR = config['MOTOR_DRIVER']['MOTORS']['RIGHT']
-
+            self.RIGHT_MOTOR = config['MOTOR_DRIVER']['MOTORS']['RIGHT']x
         if on_pi:
             GPIO.setmode(GPIO.BCM)
             time.sleep(0.1)
-            GPIO.setup(I2C_PIN, GPIO.OUT)
+            GPIO.setup(self.I2C_PIN, GPIO.OUT)
             time.sleep(0.1)
+
 
     def on(self):
         if on_pi:
-            GPIO.output(PIN_I2C6_POWER_ENABLE, GPIO.HIGH)
+            GPIO.output(self.I2C_PIN, GPIO.HIGH)
+
 
     def off(self):
         if on_pi:
-            set_motors(0, 0)
-            GPIO.output(PIN_I2C6_POWER_ENABLE, GPIO.LOW)
+            self.set_to(0, 0)
+            GPIO.output(self.I2C_PIN, GPIO.LOW)
+
 
     def set_to(self, left, right, verbose=False):
         if on_pi:
-            bus.write_i2c_block_data(DEVICE_ADDRESS,LEFT_MOTOR,util.int_to_byte_array(left))
-            bus.write_i2c_block_data(DEVICE_ADDRESS,RIGHT_MOTOR,util.int_to_byte_array(right))
+            self.BUS.write_i2c_block_data(self.DEVICE_ADDRESS,self.LEFT_MOTOR,util.int_to_byte_array(left))
+            self.BUS.write_i2c_block_data(self.DEVICE_ADDRESS,self.RIGHT_MOTOR,util.int_to_byte_array(right))
         if verbose:
             print("Left:", left)
             print("Right:", right)
