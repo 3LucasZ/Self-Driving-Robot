4a09e621241c5b05edfda97dc3440956ba9ca69f
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-10 15:37:26 -0700

    Update __main__.py

--- Classification_Data_Collection/__main__.py
+++ Classification_Data_Collection/__main__.py
@@ -38,8 +38,8 @@ socketio = SocketIO(app)
 
 #nmotor setup
 motorController = motor.MotorController()
-FORWARD_SPEED = 200
-PIVOT_SPEED = 100
+FORWARD_SPEED = 100
+PIVOT_SPEED = 50
 # 3 motor labels: 1 is left pivot, 2 is forward, 3 is right pivot
 directionID = 1
 
