6e81db16febcb34150824e5fdd50fd489f3b3665
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-12 19:52:56 -0700

    Update zip_dataset.py

--- zip_dataset.py
+++ zip_dataset.py
@@ -4,16 +4,17 @@ import sys
 import time
 
 DATASETS_PATH = os.path.join(utils.get_root(), 'Datasets')
-NUM_DATASETS = len(sys.argv) - 1
-print("Zipping", NUM_DATASETS, "datasets...")
-for i in range(NUM_DATASETS):
-    DATASET_NAME = os.path.basename(sys.argv[i + 1])
+
+for dataset in sys.argv[1:]:
+    DATASET_NAME = os.path.basename(dataset)
     DATASET_PATH = os.path.join(DATASETS_PATH, DATASET_NAME)
     ZIPPED_PATH = DATASET_PATH+'.zip'
 
-    print("Zipping:", DATASET_NAME, "to", ZIPPED_PATH)
+    print("Zipping", DATASET_NAME, "to", ZIPPED_PATH)
     time.sleep(2)
     os.chdir(DATASET_PATH)
     cmd = 'zip -r '+ZIPPED_PATH+' .'
     os.system(cmd)
-    os.chdir(utils.get_root())
+    os.chdir(DATASETS_PATH)
+
+print("Finished")
