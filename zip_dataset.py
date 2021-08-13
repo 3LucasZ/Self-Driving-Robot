a777ef6b2d0a31a55312b1bcd9ecf6ed32c1b3a4
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-12 19:40:38 -0700

    Update zip_dataset.py

--- zip_dataset.py
+++ zip_dataset.py
@@ -3,11 +3,12 @@ import os
 import sys
 
 DATASETS_PATH = os.path.join(utils.get_root(), 'Datasets')
-DATASET_NAME = os.path.basename(sys.argv[1])
-DATASET_PATH = os.path.join(DATASETS_PATH, DATASET_NAME)
-ZIPPED_PATH = DATASET_PATH+'.zip'
+for i in range(1, len(sys.argv)):
+    DATASET_NAME = os.path.basename(sys.argv[i])
+    DATASET_PATH = os.path.join(DATASETS_PATH, DATASET_NAME)
+    ZIPPED_PATH = DATASET_PATH+'.zip'
 
-os.chdir(DATASET_PATH)
-cmd = 'zip -r '+ZIPPED_PATH+' .'
-os.system(cmd)
-os.chdir(utils.get_root())
+    os.chdir(DATASET_PATH)
+    cmd = 'zip -r '+ZIPPED_PATH+' .'
+    os.system(cmd)
+    os.chdir(utils.get_root())
