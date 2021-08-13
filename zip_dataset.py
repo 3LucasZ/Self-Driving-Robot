e11692f3ab2985e7b2af44993f32db32b682207d
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-12 20:33:38 -0700

    Update zip_dataset.py

--- zip_dataset.py
+++ zip_dataset.py
@@ -8,7 +8,7 @@ DATASETS_PATH = os.path.join(utils.get_root(), 'Datasets')
 for dataset in sys.argv[1:]:
     #sometimes the dataset ends with / and messes the program up
     if dataset[-1] == '/':
-        del dataset[-1] 
+        dataset = dataset[:-1] 
     DATASET_NAME = os.path.basename(dataset)
     DATASET_PATH = os.path.join(DATASETS_PATH, DATASET_NAME)
     ZIPPED_PATH = DATASET_PATH+'.zip'
