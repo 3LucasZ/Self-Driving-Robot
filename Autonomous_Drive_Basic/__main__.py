4b242c5b7311d3c8a319273d18d33a383b0193a7
3LucasZ <lucas.j.zheng@gmail.com>
2021-08-14 13:51:01 -0700

    new circuit launch model

--- Autonomous_Drive_Basic/__main__.py
+++ Autonomous_Drive_Basic/__main__.py
@@ -24,7 +24,7 @@ import time
 
 #SETUP
 #tflite model
-modelName = 'ClassDotted2.tflite'
+modelName = 'CircuitLaunch4.tflite'
 mode = 'classification'
 model = inference.TfliteModel(modelName, mode)
 
